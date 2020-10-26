8a7a6708329ac74fa114d12c06ec962b0ae5a9f4
SouzaGiovanna <gi.souza03@gmail.com>
2020-10-26 17:07:53 -0300

    menu

--- Easychool_v4/app/src/main/java/com/cursoandroid/easychool_v4/ConfigPerfilActivity.java
+++ Easychool_v4/app/src/main/java/com/cursoandroid/easychool_v4/ConfigPerfilActivity.java
@@ -1,7 +1,9 @@
 package com.cursoandroid.easychool_v4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.Menu;
 
 public class ConfigPerfilActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class ConfigPerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_perfil);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_configurar_perfil, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
