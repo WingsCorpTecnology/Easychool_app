e43806ac0dabfe39ff88bdfa6ff7e4a2952e313e
SouzaGiovanna <gi.souza03@gmail.com>
2020-10-26 17:07:32 -0300

    layout tela de config + menu

---
+++ Easychool_v4/app/src/main/java/com/cursoandroid/easychool_v4/ConfigPerfilActivity.java
@@ -0,0 +1,13 @@
+package com.cursoandroid.easychool_v4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_perfil);
+    }
+}
